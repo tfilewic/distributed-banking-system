c34c64f69a7d1fbf98ff2c424f566ebc07eeb2e5
tfilewic <tfilewic@asu.edu>
2025-10-24 01:03:06 -0600

    Implement customer

--- grpc/customer.py
+++ grpc/customer.py
@@ -1,7 +1,17 @@
-import grpc
-import example_pb2
-import example_pb2_grpc
-import time
+"""
+customer.py
+CSE 531 - gRPC Project
+tfilewic
+2025-10-24
+
+Customer client logic and event execution.
+"""
+
+
+import banks_pb2
+import banks_pb2_grpc
+from utilities import create_channel, QUERY, WITHDRAW, DEPOSIT
+
 
 class Customer:
     def __init__(self, id, events):
@@ -14,10 +24,31 @@ class Customer:
         # pointer for the stub
         self.stub = None
 
-    # TODO: students are expected to create the Customer stub
+    
     def createStub(self):
-        pass
+        channel =  create_channel(self.id)
+        self.stub =  banks_pb2_grpc.RPCStub(channel)
 
-    # TODO: students are expected to send out the events to the Bank
+
     def executeEvents(self):
-        pass
+        for event in self.events:
+            interface = event["interface"]
+            entry = {"interface" : interface}
+            if interface == DEPOSIT:
+                request = banks_pb2.TransactionRequest(id=self.id, amount=event["money"])
+                response = self.stub.Deposit(request)
+                entry["result"] = response.result
+            elif interface == WITHDRAW:
+                request = banks_pb2.TransactionRequest(id=self.id, amount=-event["money"])
+                response = self.stub.Withdraw(request)
+                entry["result"] = response.result
+            elif interface == QUERY:
+                request = banks_pb2.BalanceRequest(id=self.id)
+                response = self.stub.Query(request)
+                entry["balance"] = response.balance
+            else: 
+                continue          
+
+            self.recvMsg.append(entry)
+        
+        return {"id": self.id, "recv": self.recvMsg}
